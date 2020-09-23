8b2a9ece168a7e0f3fffcb26136ade2c1175d091
flarsi <danil.zap63.dd@gmail.com>
2020-09-23 17:28:46 +0300

    header wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {Content} from "./components/content/Content";
+import axios from "axios";
 
 function App() {
+
+    const [user, serUser] = useState();
+
+    // axios.get('http://localhost:3001/api/v1/users')
+    //     .then((res) => {
+    //         console.log(res)
+    //     })
+
+    axios.post('http://localhost:3001/api/v1/users',
+        {
+            email: "test@gmail.com",
+            password: "passwd",
+            name: "test"
+        })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <Content/>
     </div>
   );
 }
---
+++ src/components/content/Content.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Content = () => {
+
+    return(
+        <div className="content">
+            <h1>CONTENT</h1>
+        </div>
+    )
+}
